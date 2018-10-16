e01c75f0c0ada3962c63bd76cd2578fea2ff9cb0
polyedre <lucas_henry@live.fr>
2018-10-16 08:57:30 +0200

    Cleaned main.py to use external class

--- main.py
+++ main.py
@@ -1,41 +1,25 @@
-import pygame
-import time
+import pygame as pg
 
 GAME_NAME = "test"
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-pygame.init()
+## SETUP ##
+pg.init()
+SCREEN = pg.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+pg.display.set_caption(GAME_NAME)
 
-sc = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
-pygame.display.set_caption(GAME_NAME)
+RUNNING = True
 
-sprite_list = pygame.sprite.Group()
+while RUNNING:
 
-sc.fill((255,255,255))
+    SCREEN.fill((255, 255, 255))
 
-pygame.draw.rect(sc, (100, 100, 255), [350, 450, 100, 200])
-pygame.draw.circle(sc, (100, 100, 255), [400, 500], 100)
+    pg.display.flip()
 
-running = True
-
-while running:
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             running = False
 
-    mouse = pygame.math.Vector2(pygame.mouse.get_pos())
-
-    center_player = pygame.math.Vector2(400,500)
-
-    dir_arm = (mouse - center_player).normalize() * 100
-
-    pygame.draw.line(sc, (0, 0, 0), (400,500), (center_player + dir_arm))
-
-    pygame.display.flip()
-
-    time.sleep(0.1)
-
-pygame.quit()
+pg.quit()
